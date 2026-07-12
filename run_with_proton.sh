c48a8e1bfc74609b93e36a4e667906572f9daa20
Sausa <sausa28@proton.me>
2026-07-12 02:11:07 +0100

    Allow proton run from base folder

--- run_with_proton.sh
+++ run_with_proton.sh
@@ -4,4 +4,5 @@ export STEAM_COMPAT_DATA_PATH=$HOME/.steam/steam/steamapps/compatdata/2193092650
 
 proton="$HOME/.steam/steam/steamapps/common/Proton 10.0/proton"
 
+cd game
 "$proton" run minicarracing.exe
